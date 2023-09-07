66c3b19a4cf49bf5aab3d7a987f2e5e8efccfcbd
Carlos <arlo252@gmail.com>
2023-09-07 11:39:45 -0400

    small updates

--- src/app/services/addrecord.service.ts
+++ src/app/services/addrecord.service.ts
@@ -11,11 +11,11 @@ export class AddrecordService {
   constructor(private httpClient2: HttpClient) { }
 
   registerRecord(formData2: any){
-    return this.httpClient2.post<Irecord>('http://back.swap2go.ca/addrecord/', formData2)
+    return this.httpClient2.post<Irecord>('https://back.swap2go.ca/addrecord/', formData2)
   }
 
   getRecordData2(): Observable<any> {
-    return this.httpClient2.get<any>("https://back.swap2go.ca/addrecord/")
+    return this.httpClient2.get<any>("https://back.swap2go.ca/records")
   }
 
 }
