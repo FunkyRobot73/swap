dea38698c4777fc4d1e42da9719a10c78660645d
Carlos <arlo252@gmail.com>
2023-10-25 22:48:40 -0400

    more Search stuff

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,14 +8,24 @@ import { ComicService } from 'src/app/services/comic.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  comicbook: any;
+  data: any;
+  comicbook: any; // Displayed and sorted data
+  searchTerm: string = '';
   constructor(private ComicService: ComicService) {}
 
   ngOnInit(): void {
   
     this.ComicService.getData().subscribe((data) => {
-      this.comicbook = data;
+      this.data = data;
+      this.comicbook = [...this.data]
     })
   
   }
+
+  sortData() {
+    this.comicbook = this.data.filter((item:any) => {
+      const values = Object.values(item).join('').toLowerCase();
+      return values.includes(this.searchTerm.toLowerCase());
+    });
+}
 }
